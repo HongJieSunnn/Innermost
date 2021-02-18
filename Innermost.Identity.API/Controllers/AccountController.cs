b8b942e0d73bbaf11120f8a6511e2e14fb9021aa
HongJieSunnn <457406475@qq.com>
2021-02-18 18:17:46 +0800

    Summary 参考eshopcontainer完成了登出功能。
    Add
    1. add logoutModel for logout
    
    Change
    1. add judge whether the user want to sign-in is existing in function LoginAsync

--- Innermost.Identity.API/Controllers/AccountController.cs
+++ Innermost.Identity.API/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using IdentityServer4.Services;
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Services;
 using IdentityServer4.Stores;
 using Innermost.Identity.API.Models;
 using Innermost.Identity.API.Services;
@@ -12,6 +14,7 @@ using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -43,6 +46,7 @@ namespace Innermost.Identity.API.Controllers
             _userManager = userManager;
             _configuration = configuration;
         }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> LoginAsync([FromBody] LoginModel loginModel)
@@ -50,6 +54,11 @@ namespace Innermost.Identity.API.Controllers
             if(ModelState.IsValid)
             {
                 var user = await _loginService.FindByAccount(loginModel.Account, loginModel.AccountType);
+
+                if(user==null)//用户不存在
+                {
+                    return Unauthorized("Account is not existing");
+                }
 
                 if (await _loginService.ValidateCredentials(user,loginModel.PassWord))
                 {
@@ -94,6 +103,69 @@ namespace Innermost.Identity.API.Controllers
             {
                 return modelJson;
             });
+        }
+        /// <summary>
+        /// 当登出时，IdentityServer应该会默认调用该函数,并带上logoutId
+        /// </summary>
+        /// <param name="logoutId">IdentityServer给的</param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> Logout(string logoutId)
+        {
+            if(User.Identity.IsAuthenticated==false)
+            {
+                return await Logout(new LogoutModel { LogoutId = logoutId });
+            }
+
+            var logoutContext = await _identityServerInteraction.GetLogoutContextAsync(logoutId);
+            if(logoutContext.ShowSignoutPrompt==false)//不显示确认登出界面
+            {
+                return await Logout(new LogoutModel { LogoutId = logoutId });
+            }
+            //重定向到确认登出界面，带上logoutId，然后确实登出再带上logoutId来post
+            return Redirect($"~/Account/LogoutPrompt?logoutId={logoutId}");
+        }
+        /// <summary>
+        /// 具体登出的操作
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout(LogoutModel model)
+        {
+            var idp = User?.FindFirst(JwtClaimTypes.IdentityProvider)?.Value;
+            //这种是非本地的Identity提供者，也就是例如用谷歌登陆等，需要创建一个logoutId，IdentityServer应该不会自动生成一个，只有使用本地的验证服务器才会
+            //这段是参考 eshopContainer的
+            if (idp != null && idp != IdentityServerConstants.LocalIdentityProvider)
+            {
+                if (model.LogoutId == null)
+                {
+                    model.LogoutId = await _identityServerInteraction.CreateLogoutContextAsync();
+                }
+
+                string url = "/Account/Logout?logoutId=" + model.LogoutId;
+                try
+                {
+                    await HttpContext.SignOutAsync(idp, new AuthenticationProperties
+                    {
+                        RedirectUri = url
+                    });
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "LOGOUT ERROR: {ExceptionMessage}", ex.Message);
+                }
+            }
+            //删除cookie
+            await HttpContext.SignOutAsync();
+            await HttpContext.SignOutAsync(IdentityConstants.ApplicationScheme);
+
+            //登出后设置User为一个anonymous用户
+            HttpContext.User = new System.Security.Claims.ClaimsPrincipal(new ClaimsIdentity());
+
+            var logoutContext = await _identityServerInteraction.GetLogoutContextAsync(model.LogoutId);
+            return Redirect(logoutContext?.PostLogoutRedirectUri);
         }
     }
 }
---
+++ Innermost.Identity.API/Models/LogoutModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Innermost.Identity.API.Models
+{
+    public record LogoutModel
+    {
+        public string LogoutId { get; set; }
+    }
+}
